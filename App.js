db82b2639081d9447ea1a42bee8d8ef9e6c2924a
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-16 18:21:23 +0530

    add new btn

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText]= useState('Open up App.js to start working on your app!');
+  // const [outputText, setOutputText]= useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('Text Changed')}/>
+    // <View style={styles.container}>
+    //   <Text>{outputText}</Text>
+    //   <Button title="Change Text" onPress={() => setOutputText('Text Changed')}/>
+    // </View>
+    <View>
+      <View>
+        <TextInput/>
+        <Button title="Add"/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
 });
