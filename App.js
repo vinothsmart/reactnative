38234d6ccff08cbd649546d03357b54f25f83f8b
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-18 13:47:51 +0530

    three views created

--- App.js
+++ App.js
@@ -8,36 +8,71 @@ export default function App() {
     //   <Text>{outputText}</Text>
     //   <Button title="Change Text" onPress={() => setOutputText('Text Changed')}/>
     // </View>
-    <View style={{ padding: 50 }}>
+    // <View style={{ padding: 50 }}>
+    //   <View
+    //     style={{
+    //       flexDirection: "row",
+    //       justifyContent: "space-between",
+    //       alignContent: "center",
+    //     }}
+    //   >
+    //     <TextInput
+    //       placeholder="Course Goal"
+    //       style={{
+    //         width: "80%",
+    //         borderColor: "black",
+    //         borderWidth: 1,
+    //         padding: 10,
+    //       }}
+    //     />
+    //     <Button
+    //       title="Add"
+    //     />
+    //   </View>
+    // </View>
+    <View style={{ padding: 50, }}>
       <View
         style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignContent: "center",
+          backgroundColor: "red",
+          width: 100,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <TextInput
-          placeholder="Course Goal"
-          style={{
-            width: "80%",
-            borderColor: "black",
-            borderWidth: 1,
-            padding: 10,
-          }}
-        />
-        <Button 
-          title="Add" 
-        />
+        <Text>1</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "blue",
+          width: 100,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "green",
+          width: 100,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>3</Text>
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
