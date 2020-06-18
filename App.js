94bcd42ae4a4e66e82ccbcb5d14339e9f9992be2
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-18 13:36:35 +0530

    more styles added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   // const [outputText, setOutputText]= useState('Open up App.js to start working on your app!');
@@ -8,13 +8,26 @@ export default function App() {
     //   <Text>{outputText}</Text>
     //   <Button title="Change Text" onPress={() => setOutputText('Text Changed')}/>
     // </View>
-    <View style={{padding:50}}>
-      <View style={{flexDirection: 'row'}}>
-        <TextInput 
-          placeholder="Course Goal" 
-          style={{borderColor:  'black', borderWidth: 1, padding: 10}}
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignContent: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            width: "80%",
+            borderColor: "black",
+            borderWidth: 1,
+            padding: 10,
+          }}
         />
-        <Button title="Add"/>
+        <Button 
+          title="Add" 
+        />
       </View>
     </View>
   );
