a3c7d8da75398deae9462d20f27032a1ca21b947
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-18 13:50:19 +0530

    flexDirection row-reverse

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ export default function App() {
     //     />
     //   </View>
     // </View>
-    <View style={{ padding: 50, flexDirection: 'row' }}>
+    <View style={{ padding: 50, flexDirection: 'row-reverse' }}>
       <View
         style={{
           backgroundColor: "red",
