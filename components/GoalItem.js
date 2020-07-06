fe362d04d8811a4b5eafe1795669113e7cfa2faa
vinothsmart <s.vinothkanna90@gmail.com>
2020-07-06 11:49:49 +0530

    TouchableNativeFeedback

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
+import { View, Text, StyleSheet, TouchableNativeFeedback } from "react-native";
 
 const GoalItem = (props) => {
   return (
-    <TouchableHighlight onPress={props.onDelete}>
+    <TouchableNativeFeedback underlayColor="#ccc" onPress={props.onDelete}>
       <View style={styles.listItem}>
         <Text>{props.title}</Text>
       </View>
-    </TouchableHighlight>
+    </TouchableNativeFeedback>
   );
 };
 
