5c0e8b10725607df37d11089dc53862d0afa65f4
vinothsmart <s.vinothkanna90@gmail.com>
2020-07-06 11:43:05 +0530

    touchable feedback

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
 
 const GoalItem = (props) => {
   return (
-    <TouchableOpacity activeOpacity={0.8} onPress={props.onDelete}>
+    <TouchableHighlight onPress={props.onDelete}>
       <View style={styles.listItem}>
         <Text>{props.title}</Text>
       </View>
-    </TouchableOpacity>
+    </TouchableHighlight>
   );
 };
 
