77bed46374dfc8f61e395e7bd7db521a08beeef1
vinothsmart <s.vinothkanna90@gmail.com>
2020-10-05 14:16:14 +0530

    console log debuging

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ export default function App() {
 
   const [courseGoals, setCourseGoals] = useState([]);
   const [isAddMode, setIsAddMode] = useState(false);
+  console.log("RE-RENDERING COMPONENT");
+  console.log(courseGoals);
 
   const addGoalHandler = (goalTitle) => {
     if (goalTitle.length === 0) {
@@ -21,6 +23,8 @@ export default function App() {
   };
 
   const removeGoalHandler = (goalId) => {
+    console.log("TO BE DELETED:" + goalId);
+    console.log(courseGoals);
     setCourseGoals((currentGoals) => {
       return currentGoals.filter((goal) => goal.id !== goalId);
     });
